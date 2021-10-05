d44f2d9e237586497c3b27bb790bcbc7f37a9413
QiubyZ <41333888+QiubyZ@users.noreply.github.com>
2021-10-05 21:24:45 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,15 +5,19 @@ class MENU(dict):
 		
 	def add(self, **kwargs):
 		self.number += 1
-		self.update({self.number:[kwargs.get("func"),kwargs.get("info")]})
-
+		self.update({self.number:[kwargs.get("func"), kwargs.get("args"),kwargs.get("info")]})
+		
 	def showListMenu(self):
-		for number, (func, info) in self.items():
+		for number, (func, args, info) in self.items():
 			print(f"[{number}]. {info}")
-			
+		
 	def getFunctionNumber(self, number):
 		result = self.get(number, False)
 		if(result):
-			result[0]()
+			args = result[1]
+			if(args):
+				result[0](args)
+			else:
+				result[0]()
 			return True
 		return result
